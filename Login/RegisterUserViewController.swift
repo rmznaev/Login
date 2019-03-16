ed89a97481cf8bf9b9b6dca70a115a4f1f803bec
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-03-17 03:04:39 +0400

    Sign up Cocoa Touch Class created, text fields and buttons connected with VC

---
+++ Login/RegisterUserViewController.swift
@@ -0,0 +1,48 @@
+//
+//  RegisterUserViewController.swift
+//  Login
+//
+//  Created by Ramazan Abdullayev on 3/17/19.
+//  Copyright © 2019 Ramazan Abdullayev. All rights reserved.
+//
+
+import UIKit
+
+class RegisterUserViewController: UIViewController {
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+        
+        print("Sign up button tapped")
+        
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        
+        print("Cancel button tapped")
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
